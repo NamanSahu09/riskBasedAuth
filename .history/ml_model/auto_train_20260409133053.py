5fdc5ab8eb3bc9977d2ec460c649a3efe4059118
Naman Swastik Sahu <namanswastik11@gmail.com>
2026-04-09 13:35:27 +0530

    fix: cloud DB connection

---
+++ .history/ml_model/auto_train_20260409133053.py
@@ -0,0 +1,15 @@
+import time
+import subprocess
+
+while True:
+    print("🔁 Training model...")
+
+    try:
+        subprocess.run(["python", "train_model.py"], check=True)
+        print("Training complete")
+
+    except Exception as e:
+        print("Training failed:", e)
+
+    print(" Waiting 15 seconds...\n")
+    time.sleep(15)
